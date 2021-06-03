bc93f9ab92f2f4b486fac9168e69394f78898e80
jolayemi_local <ajolayemi82@gmail.com>
2021-06-03 12:46:56 +0200

    Added logging section in create_table method logging the status of query execution

--- modules/database_writer.py
+++ modules/database_writer.py
@@ -21,10 +21,17 @@ class DatabaseWriter:
         self.db_name = db_name
         self.con_name = con_name
         self.table_name = table_name
+        self.logger_cls = logger.Logger(file_name=LOG_FILE_NAME)
 
         self.con_error = False
 
         self._create_con()
+
+    def populate_table(self, set_id: int, set_name: str,
+                       set_component: str, component_qta: int,
+                       component_netto_qta: int, component_id: int,
+                       set_code: str):
+        pass
 
     def create_table(self):
         table_query_cls = QSqlQuery(self.writer_connection)
@@ -40,6 +47,14 @@ class DatabaseWriter:
             """
         )
         table_query_cls.exec_(query)
+        if table_query_cls.isActive():
+            msg_to_log = f'{self.table_name} table was created successfully in \n' \
+                         f'{self._get_db_name()}'
+            self.logger_cls.log_info_msg(msg_to_log)
+        else:
+            msg_to_log = f'There was an error while trying to create {self.table_name} table in \n' \
+                         f'{self._get_db_name()}'
+            self.logger_cls.log_error_msg(msg_to_log)
 
     def _get_con_name(self):
         return self.writer_connection.connectionName()
@@ -49,24 +64,22 @@ class DatabaseWriter:
 
     def _create_con(self):
         """ Creates database connection """
-        # Logger class
-        logger_cls = logger.Logger(file_name=LOG_FILE_NAME)
         self.writer_connection = QSqlDatabase.addDatabase(
             self.db_driver, connectionName=self.con_name
         )
         self.writer_connection.setDatabaseName(self.db_name)
-        print(self.writer_connection.open())
         if not self.writer_connection.open():
             self.con_error = True
             msg_to_log = f'There was an error while trying to ' \
                          f'connect to database with writer connection name \n' \
                          f'{self.con_name}'
-            logger_cls.log_error_msg(msg_to_log)
+            self.logger_cls.log_error_msg(msg_to_log)
         else:
             msg_to_log = f'Writer connection - {self.con_name} - in DatabaseReader class created' \
                          f' successfully. '
-            logger_cls.log_info_msg(msg=msg_to_log)
+            self.logger_cls.log_info_msg(msg=msg_to_log)
 
 
 if __name__ == '__main__':
-    pass
+    t = DatabaseWriter()
+    t.create_table()
