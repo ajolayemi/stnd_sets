a00b8c8aa1b381af35266646ac747f2fed2e89e3
jolayemi_local <ajolayemi82@gmail.com>
2021-06-03 14:35:58 +0200

    Imported the needed modules

--- modules/database_reader.py
+++ modules/database_reader.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python
 
 """ Reads data from database. """
+
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery
+
+# Self defined modules
+from settings import (DATABASE_NAME, TABLE_NAME,
+                      READER_CON_NAME, DATABASE_DRIVER,
+                      LOG_FILE_NAME)
+from helper_modules import logger
