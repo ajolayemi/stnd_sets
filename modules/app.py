8fb70bc347756ecffbc58343b035c95bcbca0afd
jolayemi_local <ajolayemi82@gmail.com>
2021-06-05 15:16:15 +0200

    Added logs in app.py

--- modules/app.py
+++ modules/app.py
@@ -4,13 +4,27 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from views import UiWindow
+from helper_modules import helper_functions
+from settings import LOGGER_CLS, LOG_FILE_NAME
+
+with open(LOG_FILE_NAME, 'a') as file:
+    file.write(f'\n {"-" * 130}\n')
 
 
 def main():
+    LOGGER_CLS.log_info_msg(f'User ({helper_functions.get_user_name()}) started a new '
+                            f'session.')
     app = QApplication(sys.argv)
     win = UiWindow()
     win.show()
-    sys.exit(app.exec_())
+    exit_code = app.exec_()
+    if exit_code == 0:
+        msg_to_log = 'App exited with exit code of 0. Successful operations.'
+        LOGGER_CLS.log_info_msg(msg_to_log)
+    else:
+        msg_to_log = 'App exited with exit code other than 0. Unsuccessful operations'
+        LOGGER_CLS.log_error_msg(msg_to_log)
+    sys.exit(exit_code)
 
 
 if __name__ == '__main__':
